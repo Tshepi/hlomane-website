9d50dae418fcca4a9ce9c003ceb152e4ca263ab2
hlomanemag <hlomanemag@outlook.com>
2023-10-03 12:50:24 +0200

    Add S3 bucket and s3 website configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,23 @@ provider "aws" {
   profile = "default"
   region = "eu-west-1"
 }
+
+resource "aws_s3_bucket" "hlomane" {
+    bucket = "hlomane-bucket"
+    tags = {
+      Name = "hlomane-website-1"
+      Environment = "Development"
+    }
+}
+
+resource "aws_s3_bucket_website_configuration" "hlomane-website" {
+  bucket = aws_s3_bucket.hlomane.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
